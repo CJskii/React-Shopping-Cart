81a1e52fbb6dd1cc9016190a522c0753e5a71f71
CJskii <czareczarek3@gmail.com>
2023-02-12 01:43:54 +0000

    Initial commit

---
+++ my-shop/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Nav
+      Home
+      Products
+      Contact */}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
