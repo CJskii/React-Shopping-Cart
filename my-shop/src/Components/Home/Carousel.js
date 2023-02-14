964825507136f8bba7e4d901218f566bc9685c6e
CJskii <czareczarek3@gmail.com>
2023-02-14 17:53:37 +0000

    Home Styling

--- my-shop/src/Components/Home/Carousel.js
+++ my-shop/src/Components/Home/Carousel.js
@@ -2,54 +2,52 @@ import React, { useEffect, useState } from "react";
 import "../../Styles/Main.css";
 
 const Carousel = () => {
-  const [itemStatus, setItemStatus] = useState([]);
   const [displayItem, setDisplayItem] = useState([]);
   useEffect(() => {
     setDisplayItem("item1");
   }, []);
 
   const handleCarousel = (e) => {
-    const nextItem = e.target.id;
-    console.log(nextItem);
-    setDisplayItem(nextItem);
+    setDisplayItem(e.target.id);
   };
 
   return (
-    <div className="carousel">
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img src={`/images/carousel/${displayItem}.jpg`} className="w-full" />
+    <div className="carousel flex justify-center items-center">
+      <div id="item1" className="carousel-item m-8">
+        <img
+          src={`/images/carousel/${displayItem}.jpg`}
+          className="w-full carousel-item"
+        />
+        <div className="flex justify-center w-full py-2 gap-2">
+          <button
+            id="item1"
+            className="carousel-btn btn btn-xs text-white p-2"
+            onClick={(e) => handleCarousel(e)}
+          >
+            1
+          </button>
+          <button
+            id="item2"
+            className="carousel-btn btn btn-xs text-white p-2"
+            onClick={(e) => handleCarousel(e)}
+          >
+            2
+          </button>
+          <button
+            id="item3"
+            className="carousel-btn btn btn-xs text-white p-2"
+            onClick={(e) => handleCarousel(e)}
+          >
+            3
+          </button>
+          <button
+            id="item4"
+            className="carousel-btn btn btn-xs text-white p-2"
+            onClick={(e) => handleCarousel(e)}
+          >
+            4
+          </button>
         </div>
-      </div>
-      <div className="flex justify-center w-full py-2 gap-2">
-        <button
-          id="item1"
-          className="btn btn-xs text-white"
-          onClick={(e) => handleCarousel(e)}
-        >
-          1
-        </button>
-        <button
-          id="item2"
-          className="btn btn-xs"
-          onClick={(e) => handleCarousel(e)}
-        >
-          2
-        </button>
-        <button
-          id="item3"
-          className="btn btn-xs"
-          onClick={(e) => handleCarousel(e)}
-        >
-          3
-        </button>
-        <button
-          id="item4"
-          className="btn btn-xs"
-          onClick={(e) => handleCarousel(e)}
-        >
-          4
-        </button>
       </div>
     </div>
   );
--- my-shop/src/Components/Home/Home.js
+++ my-shop/src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import Carousel from "./Carousel";
 
 const Home = () => {
   return (
-    <div className="container min-h-full min-w-full grid grid-cols-4 grid-rows-4 gap-4 p-4">
+    <div className="container max-h-full min-w-full p-4">
       <Header />
       <Carousel />
     </div>
