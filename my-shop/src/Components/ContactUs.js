7ad234a31865925bb5fe7fe325f3d990260943d0
CJskii <czareczarek3@gmail.com>
2023-02-16 22:47:21 +0000

    Styling + Products img

--- my-shop/src/Components/ContactUs.js
+++ my-shop/src/Components/ContactUs.js
@@ -3,49 +3,72 @@ import React from "react";
 const ContactUs = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="mockup-window border border-base-300">
-        <div className="flex flex-col justify-center px-4 py-16 border-t border-base-300">
-          <h1>Contact Us</h1>
-          <p>
-            If you have any questions, concerns, or feedback about our products
-            or services, we're here to help. Here are some ways to get in touch
-            with us:
-          </p>
-          <h3>Email:</h3>
-          <p>
-            You can send us an email at support@cryptocave.com and we'll get
-            back to you as soon as possible. Our customer support team is
-            available 24/7 to assist you with any questions or issues you may
-            have.
-          </p>
-          <h3>Phone:</h3>
-          <p>
-            If you prefer to speak with someone over the phone, you can reach us
-            at (555) 123-4567 during our business hours, which are Monday to
-            Friday from 9:00 am to 5:00 pm EST.
-          </p>
-          <h3>Live Chat:</h3>
-          <p>
-            Our website also features a live chat option, which allows you to
-            connect with a customer service representative in real-time. Simply
-            click on the chat icon located in the bottom right corner of our
-            website to get started.
-          </p>
-          <h3>Social Media:</h3>
-          <p>
-            We're also active on social media, so you can connect with us on
-            Facebook, Twitter, and Instagram. Follow us for the latest updates
-            on our products, promotions, and company news.
-          </p>
-          <h3>In-Person:</h3>
-          <p>
-            If you're in the area, you're welcome to visit our office located at
-            1234 Main St, Anytown, USA. Please note that our office is open by
-            appointment only.
-          </p>
-          <p>
+      <div className="mockup-window border border-base-300 my-8">
+        <div className="flex flex-col justify-center items-top px-4 py-4 border-t border-base-300">
+          <div className="text-center p-4 mb-12">
+            <h1 className="text-4xl ">Contact Us</h1>
+            <p className="text-2xl">
+              If you have any questions, concerns, or feedback about our
+              products or services, we're here to help.
+              <p>Here are some ways to get in touch with us:</p>
+            </p>
+          </div>
+          <div className="text-center p-4">
+            <h3 className="text-2xl">Email:</h3>
+            <p className="text-md">
+              You can send us an email at support@cryptocave.com and we'll get
+              back to you as soon as possible.
+              <p>
+                Our customer support team is available 24/7 to assist you with
+                any questions or issues you may have.
+              </p>
+            </p>
+          </div>
+          <div className="text-center p-4">
+            <h3 className="text-2xl ">Phone:</h3>
+            <p className="text-md">
+              If you prefer to speak with someone over the phone, you can reach
+              us at (555) 123-4567 during our business hours.{" "}
+              <p>We are open Monday to Friday from 9:00 am to 5:00 pm EST.</p>
+            </p>
+          </div>
+          <div className="text-center p-4">
+            <h3 className="text-2xl ">Live chat:</h3>
+            <p className="text-md">
+              Our website also features a live chat option, which allows you to
+              connect with a customer service representative in real-time.
+            </p>
+            <p>
+              Simply click on the chat icon located in the bottom right corner
+              of our website to get started.
+            </p>
+          </div>
+          <div className="text-center p-4">
+            <h3 className="text-2xl ">Social media:</h3>
+            <p className="text-md">
+              We're also active on social media, so you can connect with us on
+              Facebook, Twitter, and Instagram.
+              <p>
+                Follow us for the latest updates on our products, promotions,
+                and company news.
+              </p>
+            </p>
+          </div>
+          <div className="text-center p-4">
+            <h3 className="text-2xl ">In person:</h3>
+            <p className="text-md">
+              If you're in the area, you're welcome to visit our office located
+              at 1234 Main St, Anytown, USA.
+              <p>Please note that our office is open by appointment only.</p>
+            </p>
+          </div>
+
+          <p className="text-center mt-12">
             We appreciate your business and strive to provide the best customer
-            service possible. Thank you for choosing CryptoCave!
+            service possible.
+          </p>
+          <p className="text-center font-bold">
+            Thank you for choosing CryptoCave!
           </p>
         </div>
       </div>
--- my-shop/src/Components/Products/Shop.js
+++ my-shop/src/Components/Products/Shop.js
@@ -5,8 +5,85 @@ import Categories from "./Categories";
 const Shop = () => {
   return (
     <div className="hero min-h-screen bg-base-200 flex justify-center items-start gap-4">
-      <Categories />
-      <FeaturedItems />
+      {/* <Categories />
+      <FeaturedItems /> */}
+      <section>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-base-content sm:text-3xl">
+              New Arrivals
+            </h2>
+
+            <p className="max-w-md mx-auto mt-4 text-gray-500">
+              Explore our range of high power ASIC miners, graphic cards and
+              cool crypto gadgets!
+            </p>
+          </header>
+
+          <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+            <li>
+              <a href="#" className="relative block group">
+                <img
+                  src="/images/gpucat.jpg"
+                  alt=""
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                  style={{ objectFit: "cover" }}
+                />
+
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white bg-black p-2">
+                    Graphic Cards
+                  </h3>
+
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                    Shop Now
+                  </span>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="relative block group">
+                <img
+                  src="/images/asiccat.png"
+                  alt=""
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                />
+
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white bg-black p-2">
+                    Crypto Gadgets
+                  </h3>
+
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                    Shop Now
+                  </span>
+                </div>
+              </a>
+            </li>
+
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <a href="#" className="relative block group">
+                <img
+                  src="/images/btc-mining.png"
+                  alt=""
+                  className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+                />
+
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white  bg-black p-2 rounded">
+                    ASIC miners
+                  </h3>
+
+                  <span className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                    Shop Now
+                  </span>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
       {/* <ShopCard />
       <ShopCard />
