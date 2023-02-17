c59052c7a4352197fce6eedd8c6ff31f526303d1
CJskii <czareczarek3@gmail.com>
2023-02-17 21:44:45 +0000

    Dynamic rendering of product cards

--- my-shop/src/Components/Products/Category/CategoryItems.js
+++ my-shop/src/Components/Products/Category/CategoryItems.js
@@ -1,12 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import fetchData from "../../../FakeDatabase/data";
+import ShopCard from "../ShopCard";
+import fetchCategoryData from "../../../FakeDatabase/categoryData";
 
-const CategoryItems = ({ match }) => {
+const CategoryItems = () => {
+  const { name } = useParams();
+
+  const [itemData, setItemData] = useState([]);
+  const [category, setCategory] = useState({});
+
   useEffect(() => {
-    console.log(match);
-  });
+    const data = fetchData(name);
+    const catData = fetchCategoryData();
+    setItemData(data);
+    setCategory(catData);
+  }, [name]);
+
   return (
-    <div>
-      <h1>Bla bla bla</h1>
+    <div className="flex flex-col justify-center items-center flex-wrap w-screen">
+      <div className="lg:py-24 p-4">
+        <h2 className="text-3xl font-bold sm:text-4xl">{name}</h2>
+        <p className="mt-4 text-gray-600">{category.description}</p>
+      </div>
+      <div className="w-screen flex flex-wrap justify-center items-center gap-4 max-sm:gap-y-2 max-sm:justify-center">
+        {itemData.map((item, index) => {
+          return <ShopCard key={index} item={item} />;
+        })}
+      </div>
     </div>
   );
 };
--- my-shop/src/Components/Products/ShopCard.js
+++ my-shop/src/Components/Products/ShopCard.js
@@ -1,23 +1,20 @@
 import React from "react";
+import "../../Styles/ShopCard.css";
 
 const ShopCard = (props) => {
+  const item = props.item;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-200 shadow-xl">
       <figure className="h-full">
         <img
-          src={props.source}
-          alt="Shoes"
-          className="h-full w-full object-cover"
+          src={item.source}
+          alt={item.name}
+          className="h-full w-full object-cover max-sm:w-50"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          GeForce RTX 4080 ROG Strix OC 16GB GDDR6X
-        </h2>
-        <p>
-          Delivers the ultra performance and features that enthusiast gamers and
-          creators demand.
-        </p>
+        <h2 className="card-title">{item.name}</h2>
+        <p>{item.description}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
